370cecc4d0031f087913e76590aaae99ee0cf5bb
Vikas <vikas.sahu75@gmail.com>
2024-09-02 17:19:08 +0530

    Added div feature

--- calc_func.py
+++ calc_func.py
@@ -5,3 +5,9 @@ def do_add(a:int, b:int):
 #Subtraction
 def do_sub(a:int, b:int):
     return a-b
+
+def do_div(a:int, b:int):
+    try:
+        return a/b
+    except ZeroDivisionError as e:
+        return "Cannot divide by zero"
